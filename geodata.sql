294ef558a9d100ef69dcf848f21dd8ecc2e731ee
cuirassier1914 <40487301+cuirassier1914@users.noreply.github.com>
2018-08-12 21:15:55 +0300

    Add files via upload

---
+++ geodata.sql
@@ -0,0 +1,112 @@
+--
+--table `geodata._countries`
+--
+
+ALTER TABLE `geodata`.`_countries`
+DROP COLUMN `title_cz`,
+DROP COLUMN `title_lv`,
+DROP COLUMN `title_lt`,
+DROP COLUMN `title_ja`,
+DROP COLUMN `title_pl`,
+DROP COLUMN `title_it`,
+DROP COLUMN `title_fr`,
+DROP COLUMN `title_de`,
+DROP COLUMN `title_pt`,
+DROP COLUMN `title_es`,
+DROP COLUMN `title_en`,
+DROP COLUMN `title_be`,
+DROP COLUMN `title_ua`,
+CHANGE COLUMN `country_id` `country_id` INT(11) NOT NULL AUTO_INCREMENT ,
+CHANGE COLUMN `title_ru` `title_ru` VARCHAR(150) NOT NULL ,
+ADD PRIMARY KEY (`country_id`),
+ADD INDEX `title_ru` (`title_ru` ASC);
+
+--
+--table `geodata._regions`
+--
+
+ALTER TABLE `geodata`.`_regions`
+DROP COLUMN `title_cz`,
+DROP COLUMN `title_lv`,
+DROP COLUMN `title_lt`,
+DROP COLUMN `title_ja`,
+DROP COLUMN `title_pl`,
+DROP COLUMN `title_it`,
+DROP COLUMN `title_fr`,
+DROP COLUMN `title_de`,
+DROP COLUMN `title_pt`,
+DROP COLUMN `title_es`,
+DROP COLUMN `title_en`,
+DROP COLUMN `title_be`,
+DROP COLUMN `title_ua`,
+CHANGE COLUMN `region_id` `region_id` INT(11) NOT NULL AUTO_INCREMENT ,
+CHANGE COLUMN `title_ru` `title_ru` VARCHAR(150) NOT NULL ,
+ADD PRIMARY KEY (`region_id`),
+ADD INDEX `title_ru` (`title_ru` ASC),
+ADD INDEX `fk_region_country_idx` (`country_id` ASC),
+ADD CONSTRAINT  `fk_region_country`
+FOREIGN KEY  (`country_id`)
+REFERENCES `geodata`.`_countries` (`country_id`)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+--
+--table `geodata._cities`
+--
+
+ALTER TABLE `geodata`.`_cities`
+DROP COLUMN `region_cz`,
+DROP COLUMN `area_cz`,
+DROP COLUMN `title_cz`,
+DROP COLUMN `region_lv`,
+DROP COLUMN `area_lv`,
+DROP COLUMN `title_lv`,
+DROP COLUMN `region_lt`,
+DROP COLUMN `area_lt`,
+DROP COLUMN `title_lt`,
+DROP COLUMN `region_ja`,
+DROP COLUMN `area_ja`,
+DROP COLUMN `title_ja`,
+DROP COLUMN `region_pl`,
+DROP COLUMN `area_pl`,
+DROP COLUMN `title_pl`,
+DROP COLUMN `region_it`,
+DROP COLUMN `area_it`,
+DROP COLUMN `title_it`,
+DROP COLUMN `region_fr`,
+DROP COLUMN `area_fr`,
+DROP COLUMN `title_fr`,
+DROP COLUMN `region_de`,
+DROP COLUMN `area_de`,
+DROP COLUMN `title_de`,
+DROP COLUMN `region_pt`,
+DROP COLUMN `area_pt`,
+DROP COLUMN `title_pt`,
+DROP COLUMN `region_es`,
+DROP COLUMN `area_es`,
+DROP COLUMN `title_es`,
+DROP COLUMN `region_en`,
+DROP COLUMN `area_en`,
+DROP COLUMN `title_en`,
+DROP COLUMN `region_be`,
+DROP COLUMN `area_be`,
+DROP COLUMN `title_be`,
+DROP COLUMN `region_ua`,
+DROP COLUMN `area_ua`,
+DROP COLUMN `title_ua`,
+DROP COLUMN `region_ru`,
+DROP COLUMN `area_ru`,
+CHANGE COLUMN `city_id` `city_id` INT(11) NOT NULL AUTO_INCREMENT ,
+ADD PRIMARY KEY (`city_id`),
+ADD INDEX `title_ru` (`title_ru` ASC),
+ADD INDEX `fk_city_country_idx` (`country_id` ASC),
+ADD CONSTRAINT  `fk_city_country`
+FOREIGN KEY  (`country_id`)
+REFERENCES `geodata`.`_countries` (`country_id`)
+ON DELETE CASCADE
+ON UPDATE CASCADE,
+ADD CONSTRAINT  `fk_city_region`
+FOREIGN KEY  (`region_id`)
+REFERENCES `geodata`.`_regions` (`region_id`)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
