835d2b43caafc4a26880a9bf7cd02797d42a54ea
cuirassier1914 <40487301+cuirassier1914@users.noreply.github.com>
2018-09-07 23:11:16 +0300

    Update lesson7-tasks.sql

--- lesson7-tasks.sql
+++ lesson7-tasks.sql
@@ -1,41 +1,67 @@
-
--- -----------------------------------------------------
--- Schema users_likes
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `users_likes` DEFAULT CHARACTER SET utf8mb4 ;
-USE `users_likes` ;
-
--- -----------------------------------------------------
--- Table `users_likes`.`users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `users_likes`.`users` (
+CREATE TABLE `users_likes`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`id`),
+  INDEX `idx_name` (`name` ASC));
 
+CREATE TABLE `users_likes`.`foto` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(256) NULL,
+  PRIMARY KEY (`id`));
 
--- -----------------------------------------------------
--- Table `users_likes`.`likes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `users_likes`.`likes` (
+CREATE TABLE `users_likes`.`comments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `text` TEXT NOT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `users_likes`.`likes` (
   `id` INT NULL AUTO_INCREMENT,
   `from_id` INT NULL,
   `to_id` INT NULL,
+  `type` ENUM('foto', 'user', 'comment') NULL,
+  `object_id` INT NULL,
   PRIMARY KEY (`id`),
-  INDEX `ind_from_id` (`from_id` ASC),
-  INDEX `ind_to_id` (`to_id` ASC),
-  CONSTRAINT `fk_from_id`
+  INDEX `idx_from` (`from_id` ASC),
+  INDEX `idx_to` (`to_id` ASC),
+  INDEX `idx_object` (`object_id` ASC),
+  CONSTRAINT `fk_from`
     FOREIGN KEY (`from_id`)
     REFERENCES `users_likes`.`users` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_to_id`
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_to`
     FOREIGN KEY (`to_id`)
     REFERENCES `users_likes`.`users` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
+
+INSERT INTO `users_likes`.`users` (`name`) VALUES ('Serge');
+INSERT INTO `users_likes`.`users` (`name`) VALUES ('Alexandra');
+INSERT INTO `users_likes`.`users` (`name`) VALUES ('Nathalie');
+INSERT INTO `users_likes`.`users` (`name`) VALUES ('Ingrid');
+INSERT INTO `users_likes`.`users` (`name`) VALUES ('Nicola');
+INSERT INTO `users_likes`.`users` (`name`) VALUES ('Fredy');
+
+INSERT INTO `users_likes`.`foto` (`name`) VALUES ('cat');
+INSERT INTO `users_likes`.`foto` (`name`) VALUES ('dog');
+INSERT INTO `users_likes`.`foto` (`name`) VALUES ('bird');
+INSERT INTO `users_likes`.`foto` (`name`) VALUES ('fox');
+INSERT INTO `users_likes`.`foto` (`name`) VALUES ('wolf');
+INSERT INTO `users_likes`.`foto` (`name`) VALUES ('elephant');
+
+INSERT INTO `users_likes`.`comments` (`text`) VALUES ('Chouette !');
+INSERT INTO `users_likes`.`comments` (`text`) VALUES ('Super !');
+INSERT INTO `users_likes`.`comments` (`text`) VALUES ('Merveilleux !');
+INSERT INTO `users_likes`.`comments` (`text`) VALUES ('Parfait !');
+INSERT INTO `users_likes`.`comments` (`text`) VALUES ('Incomparable !');
+
+INSERT INTO `users_likes`.`likes` (`from_id`, `to_id`, `type`, `object_id`) VALUES ('1', '2', 'user', '2');
+INSERT INTO `users_likes`.`likes` (`from_id`, `to_id`, `type`, `object_id`) VALUES ('2', '1', 'user', '1');
+INSERT INTO `users_likes`.`likes` (`from_id`, `to_id`, `type`, `object_id`) VALUES ('3', '1', 'foto', '4');
+INSERT INTO `users_likes`.`likes` (`from_id`, `to_id`, `type`, `object_id`) VALUES ('4', '5', 'comment', '1');
+INSERT INTO `users_likes`.`likes` (`from_id`, `to_id`, `type`, `object_id`) VALUES ('5', '1', 'user', '1');
+INSERT INTO `users_likes`.`likes` (`from_id`, `to_id`, `type`, `object_id`) VALUES ('1', '5', 'foto', '2');
+INSERT INTO `users_likes`.`likes` (`from_id`, `to_id`, `type`, `object_id`) VALUES ('1', '5', 'user', '5');
 
 
 --1
@@ -57,7 +83,7 @@ LEFT JOIN likes ON users.id=likes.to_id
 GROUP BY  users.id;
 
 DROP VIEW IF EXISTS mutual;
-CREATE VIEW mutual AS 
+CREATE VIEW mutual AS
 #выбрали только взаимные---
 SELECT B.from_id, B.to_id FROM likes A
 JOIN likes B ON
@@ -65,69 +91,15 @@ B.to_id=A.from_id
 and
 B.from_id=A.to_id;
 
-SELECT users.id, users.name, 
-received.`likes received`, 
-sent.`likes sent`, 
+SELECT users.id, users.name,
+received.`likes received`,
+sent.`likes sent`,
 count(mutual.from_id) as `mutual`
 FROM users
 LEFT JOIN received  ON users.id=received.id
 LEFT JOIN sent ON users.id=sent.id
 LEFT JOIN mutual ON users.id=mutual.from_id
 GROUP BY  users.id;
-
-
------------------2---------------------
-
-use users_likes;
-
-DROP VIEW IF EXISTS C;
-CREATE VIEW C AS
-SELECT * FROM likes WHERE to_id=3;
-
-drop view if exists A;
-CREATE VIEW A AS
-SELECT from_id FROM likes
-WHERE to_id=5;
-
-drop view if exists B;
-create view B as
-select from_id from likes
-where to_id=4;
-
-drop view if exists AB;
-
-create view AB as
-SELECT A.from_id
-FROM A
-INNER JOIN B ON
-A.from_id=B.from_id
-GROUP BY A.from_id;
-
-drop view if exists ABC;
-
-create view ABC as
-select AB.from_id, C.from_id as Cfrom_id, C.to_id from AB
-left join C on AB.from_id=C.from_id;
-
-select from_id as usersID from ABC where Cfrom_id is null;
-
--------------------3-----------------------
-
-ALTER TABLE `users_likes`.`likes`
-ADD COLUMN `type` ENUM('user', 'foto', 'comment') NOT NULL AFTER `to_id`,
-ADD INDEX `ind_type` (`type` ASC);
-
-ALTER TABLE `users_likes`.`likes`
-ADD COLUMN `object_id` INT(11) NOT NULL AFTER `type`;
-
-CREATE TABLE `users_likes`.`foto` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
-
-ALTER TABLE `users_likes`.`foto`
-CHANGE COLUMN `name` `text` VARCHAR(256) NULL DEFAULT NULL , RENAME TO  `users_likes`.`comments` ;
-
 
 ---- ---------------исключение повторных лайков-----------------
 
@@ -153,7 +125,7 @@ CREATE TRIGGER trig_likes_validator
       object_id=NEW.object_id) THEN TRUE
       ELSE FALSE
       END);
-      
+
 	SET NEW.from_id=(
       CASE
       WHEN @a=TRUE
@@ -196,7 +168,7 @@ GROUP BY likes.id;
 
 SELECT * FROM `objects_users`;
 
-SELECT object_id, 
-COUNT(id) as `likes quantity`, 
-GROUP_CONCAT(DISTINCT `name` SEPARATOR ', ') as `by users` 
+SELECT object_id,
+COUNT(id) as `likes quantity`,
+GROUP_CONCAT(DISTINCT `name` SEPARATOR ', ') as `by users`
 FROM object_users GROUP BY object_id;
