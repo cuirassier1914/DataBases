1d0d1ba69b00846785142f6e1ba83f11f726e083
cuirassier1914 <s.pachkov@mail.ru>
2019-02-05 23:35:09 +0300

    added F2

--- forBranch2.php
+++ forBranch2.php
@@ -5,3 +5,15 @@
  * Date: 05.02.2019
  * Time: 23:28
  */
+class Controller
+{
+    public function NewFunction()
+    {
+        return 'newFunction';
+    }
+
+    public function F2($a)
+    {
+        $b = $a;
+    }
+}
